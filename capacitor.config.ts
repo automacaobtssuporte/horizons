d2321a38b67f51761366b86a25ea416dba7ba962
Gabriel <gabrielegydio10@gmail.com>
2025-08-14 09:27:53 -0300

    change appname

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'br.com.userbrasil.USER-BRASIL',
-  appName: 'acougue-lucrativo',
+  appName: 'acouguelucrativo',
   webDir: 'dist'
 };
 
